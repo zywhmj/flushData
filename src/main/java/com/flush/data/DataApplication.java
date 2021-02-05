56e29df6261b8641aa87ba58fb9ad02629779aa8
zyw <1158998912@qq.com>
2021-02-05 17:14:32 +0800

    启动不加载dataSource配置

--- src/main/java/com/flush/data/DataApplication.java
+++ src/main/java/com/flush/data/DataApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class) //不加载datasource配置
 public class DataApplication {
 
     public static void main(String[] args) {
